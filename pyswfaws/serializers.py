ed36af800729f39b90524475c6765842de999882
Brian Stempin <brian.stempin@gmail.com>
2015-10-22 20:24:14 -0400

    Adding new methods to serializer class and impls; adding abstract base class bits

--- pyswfaws/serializers.py
+++ pyswfaws/serializers.py
@@ -1,33 +1,64 @@
 import json
 
+from abc import ABCMeta, abstractmethod
+
 from google.protobuf import message
 
 
-class Serializer:
+class Serializer(metaclass=ABCMeta):
     """
     Defines how to serialize/unserialize messages used in SWF communications
     """
-    # TODO This interface needs an update.  When storing results, they are a messsage.  When storing arguments to a
-    # function, they're something else.  This interface needs to reflect that with additional methods and args.
 
-    def deserialize(self, message_string):
+    @abstractmethod
+    def deserialize_input(self, raw_inputs):
         """
-        Handles deserialization of the message
-        :return: a message object
-        """
-        raise NotImplementedError()
+        Deserializes a function input as a tuple with (args, kwargs).
 
-    def serialize(self, message):
+        Example of how this function might be used:
+        a = some_serializer.deserialize_input(raw_input)
+        some_func(*a[0], **a[1])
+        :param raw_inputs:
+        :return:
         """
-        Handles serialization of the message
-        :return: a string representing the message
+        pass
+
+    @abstractmethod
+    def deserialize_result(self, raw_result):
         """
-        raise NotImplementedError()
+        Deserializes a single-value result
+        :param raw_result:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def serialize_input(self, args, kwargs):
+        """
+        Serializes the input to a function.
+
+        :param args: an iterable of values
+        :param kwargs: a dict of values
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def serialize_result(self, result):
+        """
+        Serializes a single-value result
+        :param result:
+        :return:
+        """
+        pass
 
 
 class ProtobufSerializer(Serializer):
     """
     Handles serialization/deserializaton of protobuf messages
+
+    Both inputs and results are expected to be a single protobuf object.  That is, if you're using a protobuf object
+    as input, then it may appear in teh args or kwargs, but not both.  There must be exactly one argument.
     """
 
     def __init__(self, message_class):
@@ -37,32 +68,40 @@ class ProtobufSerializer(Serializer):
                             "google.protobuf.message.Message")
         self._message_class = message_class
 
-    def deserialize(self, message_string):
-        if message_string is None:
-            return None
+    def deserialize_input(self, raw_inputs):
+        return self.deserialize_result(raw_inputs)
+
+    def deserialize_result(self, raw_result):
         deserialized_message = self._message_class()
-        deserialized_message.ParseFromString(message_string)
-        result = (list(), dict())
-        result[0].append(deserialized_message)
-        return result
+        deserialized_message.ParseFromString(raw_result)
+        return deserialized_message
 
-    def serialize(self, message):
+    def serialize_input(self, args=None, kwargs=None):
+        args_len = 0
+        kwargs_len = 0
+        if args:
+            args_len = len(args)
+        if kwargs:
+            kwargs_len = len(kwargs)
 
-        if message is None:
-            return None
-        # TODO THIS IS A TERRIBLE HACK
-        if isinstance(message, tuple):
-            if len(message[0]) + len(message[1]) > 1:
-                raise Exception('Protobuf serializer can only serialize one argument.')
-            if len(message[0]) > 0:
-                serialized_message = message[0][0].SerializeToString()
-            elif len(message[1]) > 0:
-                serialized_message = message[1].items()[0].SerializeToString()
-            else:
-                raise Exception('No arguments were passed')
-        else:
-            serialized_message = message.SerializeToString()
-        return serialized_message
+        total_len = args_len + kwargs_len
+        if total_len == 0:
+            raise Exception('Serializer cannot be called with 0 arguments')
+        if total_len > 1:
+            raise Exception('Protobuf serializer cannot have more than one argument for an input')
+
+        # Because we checked the total length of all of the args, these two things are mutually exclusive
+        if args_len > 0:
+            message = args[0]
+        if kwargs_len > 0:
+            message = kwargs.items()[0]
+
+        return message.SerializeToString()
+
+    def serialize_result(self, result):
+        if result is None:
+            raise Exception('Serializer cannot serialize a Nonetype result')
+        return result.SerializeToString()
 
 
 class JsonSerializer(Serializer):
@@ -73,8 +112,14 @@ class JsonSerializer(Serializer):
     def __init__(self):
         pass  # Nothing to do here
 
-    def deserialize(self, message_string):
-        return json.loads(message_string)
+    def deserialize_input(self, raw_inputs):
+        return json.loads(raw_inputs)
 
-    def serialize(self, message):
+    def deserialize_result(self, raw_result):
+         return json.loads(raw_result)
+
+    def serialize_input(self, args, kwargs):
+        return json.dumps((args, kwargs))
+
+    def serialize_result(self, result):
         return json.dumps(message)
